31c5453f135cafd2b77146ab7bcda9813254b308
LocJayMP <locojaymp@dev>
2011-08-03 12:11:02 -0400

    adding setup.py + basename for image

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='pweave',
+      version='0.1',
+      description='',
+      author='',
+      author_email='',
+      url='',
+      packages=['pweave'],
+      license=['GPL'],
+      scripts=['pweave/pweave'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Text Processing :: Markup',
+        'License :: OSI Approved :: GNU General Public License (GPL)'],
+      long_description = """
+
+About pweave
+-------------
+
+"""
+
+)
